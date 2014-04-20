0e4a2728d8b57d8c46bdd6ae5e737397768ebf8d
Qi Cao <caoqisd@gmail.com>
2014-04-19 23:33:38 -0700

    use bootstrap to build the website

---
+++ includes/layouts/footer.php
@@ -0,0 +1,21 @@
+		<!-- row 4 -->
+		<footer class= "row">
+			<p>
+			Copyright©<?php echo date("Y"); ?>,  Nova Mobile Cloud Service. All rights reserved.	
+			</p>
+		</footer>
+	</div> <!-- end container -->
+
+	<!-- javascript -->
+		<script src="http://code.jquery.com/jquery-latest.min.js"></script>
+    	<script src="../public/stylesheets/js/bootstrap.min.js"></script>
+
+	</body>
+</html>
+
+<?php
+  // 5. Close database connection
+//	if (isset($connection)) {
+//	  mysqli_close($connection);
+//	}
+?>
--- public/index.php
+++ public/index.php
@@ -3,9 +3,51 @@
 
 <html lang="en">
 	<head>
+		<meta charset="utf-8">
+	    <title>Nova MIaaS - Mobile Cloud Infrastruture Services</title>
+	    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	    <!-- Bootstrap -->
+	    <link href="stylesheets/css/bootstrap.min.css" rel="stylesheet">
+	    <link href="stylesheets/css/custom.css" rel="stylesheet">
+	    <script src="stylesheets/js/respond.js"></script>
 	</head>
 	
 	<body>
-    	<?php echo "Welcom to Nava MIaaS!"; ?> <br />
-    </body>
-</html>
+		<div class="container">   <!-- will be end in footer.php -->
+			<!-- row 1 -->
+			<header class="row">
+		    	<div class="col-lg-3 col-md-3">
+		        	<a href="index.php"><img src="image/logosmall.png" alt="click for home." class="img-responsive"></a>
+		        </div>			    	
+		        <div>
+				   	<ul class="nav navbar-nav navbar-right">
+				        <li><a href="#" >Sign Up </a></li>
+				       	<li><a href="#" >Sign In </a></li>
+				       	<li><a href="#" >Admin </a></li>
+				    </ul>  
+			    </div>
+			</header>
+
+			<!-- row 2 -->
+		    <div class="jumbotron">
+		    	<img src="image/cloud.png" alt="" class="pull-right  img-responsive img-circle">
+		        <h2> What is Nova MIaaS ?</h2>
+		        <p>Nova MIaaS, Nova Mobile Cloud Infrastructure-as-a-Service, provides mobiles resources to users on demand in cloud infrastructure level. </br>
+		        
+		        <span class="glyphicon glyphicon-arrow-right"></span> Eliminate your need to invest in hardware up front </br>
+		        <span class="glyphicon glyphicon-arrow-right"></span> Scale up or down to handle changes in requirements </br>
+		        <span class="glyphicon glyphicon-arrow-right"></span> Develop and deploy applications faster </br>
+		        <span class="glyphicon glyphicon-arrow-right"></span> Launch as many virtual servers as you need </br>
+		        </p>
+
+		        <p><a href="#" class="btn btn-warning">Read more >></a></p>
+		    </div>
+
+		    <!-- row 3 -->
+		    <div class="col-lg-offset-4" >
+				<a class="btn btn-warning" href="#"> Get Started for Free</a> 
+				<span>Launch virtual mobiles in minutes.</span>
+			</div>
+
+
+<?php include("../includes/layouts/footer.php"); ?>
