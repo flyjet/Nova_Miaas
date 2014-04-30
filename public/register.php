66356e6b5f7f2b5a9cedca6ff8190d0bcb561982
lingzhang-lyon <zhangling.ice@gmail.com>
2014-04-29 22:31:54 -0700

    adminside user bill, modified readmore, modified register

--- public/register.php
+++ public/register.php
@@ -1,3 +1,44 @@
+<?php include("../includes/initialize.php"); ?>
+<?php
+
+if (isset($_POST['submit'])) {
+  // Process the form 
+  // validations
+  $required_fields = array("fname","lname","email","password","repassword");
+  ValidationHelper::validate_presences($required_fields);  
+  if (empty(Session::$errors)) {   
+    // Perform Insert
+    $firstname = BasicHelper::escape_value($_POST["fname"]);
+	$lastname = BasicHelper::escape_value($_POST["lname"]);
+	$email = BasicHelper::escape_value($_POST["email"]);
+    $password = BasicHelper::escape_value($_POST["password"]);
+	$repassword =BasicHelper::escape_value($_POST["repassword"]);
+	
+    if($password!=$repassword){
+    	$SESSION["message"] = "Your Password doesn't match";
+    }
+	else{	
+	    $result = UserManager::insert_user($firstname, $lastname, $email,$password);
+   
+	    if ($result) {
+			// Success
+			$found_user = UserManager::find_user_by_email($email);
+			$_SESSION["message"] = "Your account has been created.";
+			$_SESSION["user_id"] = $found_user["id"];
+			$_SESSION["email"] = $found_user["email"];
+			$_SESSION["first_name"] = $found_user["first_name"];
+			BasicHelper::redirect_to("register_c.php");
+	    } else {
+	      // Failure
+	      $_SESSION["message"] = "Your contact information update failed.";
+	    }
+	}
+  
+  }
+} 
+
+
+?>
 <?php include("../includes/layouts/header.php"); ?>
 			<!-- row 1 start in header -->
 
@@ -11,8 +52,11 @@
 
 			<!-- row 2 -->
 			<div class="row">
+			<form action="register.php" method="post">
 				<div class="col-lg-offset-1 col-lg-6" >  
 		        	<h1orange> Create a New Account </h1orange>
+				    <?php echo Session::message(); ?>
+				    <?php echo Session::form_errors(Session::$errors); ?>					
 		        	<h4>Please use the form below to create login credentials.</h4> <br>	
 		        		<div class="form-group">
 			        		<label for="fname">First Name </label> <br>    		
@@ -35,19 +79,20 @@
 			            <br>
 			        	<div class="form-group">
 			        		<label for="password">Password </label> <br>    		
-			        		<input class="col-lg-6 " type="text" name="Password" placeholder="Password">
+			        		<input class="col-lg-6 " type="text" name="password" placeholder="Password">
 			        	</div>
 
 			        	<br>
 			        	<div class="form-group">
 			        		<label for="password">Type again </label> <br>    		
-			        		<input class="col-lg-6 " type="text" name="Password" placeholder="Password">
+			        		<input class="col-lg-6 " type="text" name="repassword" placeholder="Password">
 			        	</div>
 			   
 		        	<br><br>
-		        	<a class="btn btn-warning" href="register_c.php">Continue</a>
+		        	<input class="btn btn-warning" type="submit" name="submit" value="Sign Up" />
 
 	         	</div>
+			</form>
 
 	         	<div class=" col-lg-5">
 	         		<img src="image/cloud.png" alt="" >
