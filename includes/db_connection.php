b7f25d1fa8efc0e752ffd6b9941ba809a74f435b
Qi Cao <caoqisd@gmail.com>
2014-05-12 00:12:29 -0700

    change sql ip to 127

--- includes/db_connection.php
+++ includes/db_connection.php
@@ -1,11 +1,7 @@
 <?php
-<<<<<<< HEAD
+
 	//define("DB_SERVER", "54.200.72.126");
   define("DB_SERVER", "127.0.0.1");
-=======
-	//define("DB_SERVER", "127.0.0.1");
-	define("DB_SERVER", "54.200.72.126");
->>>>>>> f54567b5836bdf888f0f4834f54a69dfa81ce3cc
 	define("DB_USER", "group6");
 	define("DB_PASS", "sjsugroup6");
 	define("DB_NAME", "nova_miaas");
